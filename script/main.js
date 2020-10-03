8f204ef61c13f72516bcc72f2f8762d4dd5e9e25
My Name <myEmail@example.com>
2020-10-03 19:08:57 +0300

    dey 4

--- script/main.js
+++ script/main.js
@@ -1,43 +1,42 @@
 'use strict';
 
-// day 1
+// получаем элементы
+
 const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
 const btnClose = document.querySelector('.btn-close');
-const catalogList = document.querySelector('.catalog-list');
 const subCatalog = document.querySelector('.subcatalog');
-const subcatalogHeader = document.querySelector('.subcatalog-header');
+const subCatalogHeader = document.querySelector('.subcatalog-header');
 const btnReturn = document.querySelector('.btn-return');
 
 
 const overlay = document.createElement('div');
 overlay.classList.add('overlay');
-document.body.insertAdjacentElement('beforeend', overlay);
+document.body.insertAdjacentElement('beforebegin', overlay);
 
-
+// function
 const openMenu = () => {
-    catalog.classList.add('open');
-    overlay.classList.add('active');
+   catalog.classList.add('open');
+   overlay.classList.add('active');
 };
 
 const closeMenu = () => {
-    closeSubMenu();
-    catalog.classList.remove('open');
-    overlay.classList.remove('active');
+   closeSubMenu();
+   catalog.classList.remove('open');
+   overlay.classList.remove('active');      
 };
 
-const openSubMenu = (event) => {
-    event.preventDefault();
-    const target = event.target;
-    const itemList = target.closest('.catalog-list__item');
-    if (itemList) {
-        subcatalogHeader.innerHTML = itemList.textContent;
-        subCatalog.classList.add('subopen');
-    };
+const openSubMenu = event => {
+   event.preventDefault();   
+   const itemList = event.target.closest('.catalog-list__item');
+   if (itemList) {
+      subCatalogHeader.innerHTML = itemList.innerHTML;
+      subCatalog.classList.add('subopen');
+   }
 };
 
 const closeSubMenu = () => {
-    subCatalog.classList.remove('subopen');
+   subCatalog.classList.remove('subopen');
 }
 
 btnBurger.addEventListener('click', openMenu);
@@ -45,9 +44,3 @@ btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 catalog.addEventListener('click', openSubMenu);
 btnReturn.addEventListener('click', closeSubMenu);
-
-document.addEventListener('keydown', (event) => {
-    if (event.code === 'Escape'){
-        closeMenu();
-    };
-});
